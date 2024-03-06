bfd1d126dbf367625edeb38ab2d5c841b2600a53
samuel zewde <h4ck4n0nyt@gmail.com>
2024-03-06 19:33:16 +0300

    exercise 1.14

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Statistics from './components/Statistics'
-// import Button from './components/Button'
+import Button from './components/Button'
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -11,17 +11,16 @@ const App = () => {
   const increaseNeutral = () => setNeutral(neutral + 1)
   const increaseBad = () => setBad(bad + 1)
   const getTotal = () => good + neutral + bad
-  const getAverage = ()=> getTotal() / 3
+  const getAverage = () => getTotal() / 3
 
   return (
     <div>
       <h1>give feedback</h1>
-
-      {/* <Button onClick={increaseGood} buttonName={good}/> */}
-      {/* <Button onClick={increaseNeutral} buttonName={neutral}/> */}
-      {/* <Button onClick={increaseBad} buttonName={bad}/> */}
+      <Button onClick={increaseGood} buttonName={"good"} />
+      <Button onClick={increaseNeutral} buttonName={"neutral"} />
+      <Button onClick={increaseBad} buttonName={"bad"} />
       <h1>statistics</h1>
-      {getTotal()?<Statistics good={good} neutral={neutral} bad={bad} getAverage={getAverage} getTotal={getTotal}/>:<p>No feedback given</p>}
+      {getTotal() ? <Statistics good={good} neutral={neutral} bad={bad} getAverage={getAverage} getTotal={getTotal} /> : <p>No feedback given</p>}
     </div>
   )
 }
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -3,12 +3,38 @@ import StatisticLine from "./StatisticLine";
 const Statistics = ({ good, neutral, bad, getAverage, getTotal }) => {
   return (
     <>
-      <StatisticLine text={"good"} value={good} />
+    <table>
+      <tr>
+        <td>good</td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>neutral</td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+      </tr>
+      <tr>
+        <td>total</td>
+        <td>{good + neutral + bad}</td>
+      </tr>
+      <tr>
+        <td>average</td>
+        <td>{getAverage()}</td>
+      </tr>
+      <tr>
+        <td>positive</td>
+        <td>{(good * 100) / getTotal()}</td>
+      </tr>
+    </table>
+      {/* <StatisticLine text={"good"} value={good} />
       <StatisticLine text={"neutral"} value={neutral} />
       <StatisticLine text={"bad"} value={bad} />
       <StatisticLine text={"total"} value={good + neutral + bad} />
       <StatisticLine text={"average"} value={getAverage()} />
-      <StatisticLine text={"positive"} value={(good * 100) / getTotal()} />
+      <StatisticLine text={"positive"} value={(good * 100) / getTotal()} /> */}
     </>
   );
 };
