7e05f08b14565e66130b3e8c2f1a0af70fef453c
samicsc0 <h4ck4n0nyt@gmail.com>
2024-03-11 00:17:25 +0300

    phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,36 +1,48 @@
-const http = require('http')
+const http = require("http");
 const data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+http
+  .createServer((request, response) => {
+    if (request.url === "/api/persons") {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(data));
+    } else if (request.url === "/info") {
+      response.writeHead(200, { "Content-Type": "text/HTML" });
+      response.end(
+        `<p>Phone book has info for ${
+          data.length
+        } people</p> <br /> <p>${new Date()}</p>`
+      );
+    } else if (request.url.startsWith("/api/person")) {
+      const id = request.url.split("/");
+      const extractedId = Number(id[id.length - 1]);
+      if (extractedId <= 0 || data.length < extractedId || isNaN(extractedId)) {
+        response.writeHead(404, { "Content-Type": "text/HTML" });
+        response.end("<p>Not Found</p>");
+      } else {
+        response.writeHead(200, { "Content-Type": "application/JSON" });
+        response.end(JSON.stringify(data[extractedId - 1]));
+      }
     }
-]
-http.createServer((request,response)=>{
-    if(request.url === '/api/persons'){
-        response.writeHead(200,{'Content-Type':'application/json'})
-        response.end(JSON.stringify(data))
-    }else if(request.url === '/info'){
-        response.writeHead(200, {'Content-Type':'text/HTML'})
-        response.end(`<p>Phone book has info for ${data.length} people</p> <br /> <p>${new Date()}</p>`)
-    }else if(request.url === '/api/person/:id'){
-        const id = request.url
-        response.writeHead(200,{'Content-Type':'application/JSON'})
-        response.end(JSON.stringify())
-    }
-}).listen(3000)
+  })
+  .listen(3000);
